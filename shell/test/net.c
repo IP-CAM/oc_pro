cdc5675da7ab3bdf193c9b70666cf0f2ff5a7536
tvrc4m <tvrc4m@qq.com>
2015-04-22 11:22:28 +0800

    remove c related files

--- shell/test/net.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <netdb.h>
-
-int main(int argc,char** argv){
-
-	struct hostent *ent=gethostbyname("local.yicker");
-	printf("%s\n", ent->h_addr_list[0]);
-	printf("%d\n", ent->h_addrtype);
-}
